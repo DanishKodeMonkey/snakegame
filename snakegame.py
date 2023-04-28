4005b67df0988707a9742c1f3b5247928c202a45
DanishKodeMonkey Hacktop <humberjini@hotmail.com>
2023-04-28 08:38:53 +0200

    For now, just some commentation text of what I wanna achieve, time to crunch some tutorials and get an idea of how to pull this off!

---
+++ snakegame.py
@@ -0,0 +1,7 @@
+'''
+The goal of this project is to create a simple game of snake
+using GUI to create a nice user interface, 2d animations using shapes
+and various functions and methods to give the user control of the game
+and score the progress. GOOD LUCK ME!
+'''
+
